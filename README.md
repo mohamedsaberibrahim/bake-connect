89d3f4e3c85ba0f9b5eb301e853ac89429033a06
Mohamed Saber <mohamed.saber@bosta.co>
2023-10-19 19:50:15 +0300

    feat: add poetry setup for managing deps

--- README.md
+++ README.md
@@ -63,7 +63,7 @@ Bake & Connect implementation raodmap steps starting from delivering a minimum v
 - [ ] Securing the application
   - [ ] Completing JWT authentication (Refresh token + forget password)
 - [ ] Shaping into Production
-  - [ ] Adding requirements.txt
+  - [x] Use dependency management tool [Poetry](https://python-poetry.org/)
   - [ ] Dockerizing the application
   - [ ] Manual deployment on Kubernetes
 - [ ] Making development cycle easier
--- app/api/users/models.py
+++ app/api/users/models.py
@@ -3,8 +3,6 @@ from sqlalchemy import Column
 from sqlalchemy.sql.sqltypes import String, LargeBinary, Boolean, Integer
 from sqlalchemy.sql.schema import UniqueConstraint, PrimaryKeyConstraint
 from app.db.base import Base
-import bcrypt
-from jose import jwt
 
 from app.settings import settings
 
@@ -29,28 +27,3 @@ class User(Base):
         """Returns string representation of model instance"""
         return "<User {name!r}>".format(name=self.name)
     
-    @staticmethod
-    def hash_password(password) -> str:
-        """Transforms password from it's raw textual form to 
-        cryptographic hashes
-        """
-        return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-
-    def validate_password(self, password) -> bool:
-        """Confirms password validity"""
-        return {
-            "access_token": jwt.encode(
-                {"name": self.name, "email": self.email},
-                "ApplicationSecretKey"
-            )
-        }
-
-    def generate_token(self) -> dict:
-        """Generate access token for user"""
-        return {
-            "access_token": jwt.encode(
-                {"name": self.name, "email": self.email},
-                settings.SECRET_KEY
-            )
-        }
-        
